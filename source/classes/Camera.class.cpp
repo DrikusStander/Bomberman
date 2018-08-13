e929845409107048d79bd1501c81e334b0bb7e13
Cornelius NOLTE <cnolte@c1r4s2.wethinkcode.co.za>
2018-08-13 20:19:57 +0200

    fix assignment operator function.

--- source/classes/Camera.class.cpp
+++ source/classes/Camera.class.cpp
@@ -6,7 +6,7 @@
 /*   By: cnolte <cnolte@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 11:26:34 by cnolte            #+#    #+#             */
-/*   Updated: 2018/08/13 18:48:43 by cnolte           ###   ########.fr       */
+/*   Updated: 2018/08/13 20:18:28 by cnolte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,19 @@ Camera::~Camera(void)
 Camera	&Camera::operator=(Camera const &rhs)
 {
 	std::cout << "Camera - Assignment operator called" << std::endl;
-	// if (this != &rhs)
-	// 	this->_foo = rhs.getfoo();
+	if (this != &rhs)
+	{
+		this->position = rhs.position;
+		this->front = rhs.front;
+		this->up = rhs.up;
+		this->right = rhs.right;
+		this->worldUp = rhs.worldUp;
+		this->yaw = rhs.yaw;
+		this->pitch = rhs.pitch;
+		this->movementSpeed = rhs.movementSpeed;
+		this->mouseSensitivity = rhs.mouseSensitivity;
+		this->zoom = rhs.zoom;
+	}
 	return (*this);
 }
 //end canonical form
