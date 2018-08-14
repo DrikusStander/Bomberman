511fe14c5cd97fce4782fe6caa3db02248a167d5
Cornelius NOLTE <cnolte@c1r4s2.wethinkcode.co.za>
2018-08-14 13:27:03 +0200

    remove white-spaces in code.

--- source/main.cpp
+++ source/main.cpp
@@ -21,183 +21,189 @@
 // Other Libs
 #include "SOIL2.h"
 
-// Properties
-const GLuint WIDTH = 2500, HEIGHT = 1350;
-int SCREEN_WIDTH, SCREEN_HEIGHT;
+// Screen Size
+const GLuint	WIDTH = 2500;
+const GLuint	HEIGHT = 1350;
+int				SCREEN_WIDTH;
+int				SCREEN_HEIGHT;
 
 // Function prototypes
-void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode );
-void MouseCallback( GLFWwindow *window, double xPos, double yPos );
-void DoMovement( );
+void	KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode );
+void	MouseCallback( GLFWwindow *window, double xPos, double yPos );
+void	DoMovement( );
 
 // Camera
-Camera camera;
-bool keys[1024];
-GLfloat lastX = 400, lastY = 300;
-bool firstMouse = true;
+Camera	camera;
+bool	keys[1024];
+GLfloat	lastX = 400;
+GLfloat	lastY = 300;
+bool	firstMouse = true;
 
-GLfloat deltaTime = 0.0f;
-GLfloat lastFrame = 0.0f;
+GLfloat	deltaTime = 0.0f;
+GLfloat	lastFrame = 0.0f;
 
-int main( )
+int	main(void)
 {
-	camera.ProcessMouseMovement( 0, -250 );
+	camera.ProcessMouseMovement(0, -250);
+
 	// Init GLFW
-	glfwInit( );
+	glfwInit();
+
 	// Set all the required options for GLFW
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
-	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
-	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-	glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
-	glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
-	GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr );
+	GLFWwindow	*window = glfwCreateWindow(WIDTH, HEIGHT, "Bomberman", nullptr, nullptr);
 
-	if ( nullptr == window )
+	if (nullptr == window)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate( );
-		
-		return EXIT_FAILURE;
+		glfwTerminate();
+
+		return (EXIT_FAILURE);
 	}
 
-	glfwMakeContextCurrent( window );
+	glfwMakeContextCurrent(window);
 
-	glfwGetFramebufferSize( window, &SCREEN_WIDTH, &SCREEN_HEIGHT );
+	glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
 
 	// Set the required callback functions
-	glfwSetKeyCallback( window, KeyCallback );
-	glfwSetCursorPosCallback( window, MouseCallback );
+	glfwSetKeyCallback(window, KeyCallback);
+	glfwSetCursorPosCallback(window, MouseCallback);
 
 	// GLFW Options
-	glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
+
 	// Initialize GLEW to setup the OpenGL Function pointers
-	if ( GLEW_OK != glewInit( ) )
+	if (GLEW_OK != glewInit())
 	{
 		std::cout << "Failed to initialize GLEW" << std::endl;
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
 
 	// Define the viewport dimensions
-	glViewport( 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT );
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	// OpenGL options
-	glEnable( GL_DEPTH_TEST );
+	glEnable(GL_DEPTH_TEST);
 
 	// Setup and compile our shaders
-	Shader shader( "resources/shaders/modelLoading.vert", "resources/shaders/modelLoading.frag" );
+	Shader	shader("resources/shaders/modelLoading.vert", "resources/shaders/modelLoading.frag");
 
 	// Load models
-	// Model ourModel( "res/models/nanosuit.obj" );
+	// Model ourModel("res/models/nanosuit.obj");
 	// Load world
-	Model world("resources/models/world.obj");
+	Model	world("resources/models/world.obj");
 
 	// Draw in wireframe
-	//glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+	// glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 
-	glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 1000.0f );
+	glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 1000.0f);
 
 	// Game loop
-	while( !glfwWindowShouldClose( window ) )
+	while (!glfwWindowShouldClose(window))
 	{
 		// Set frame time
-		GLfloat currentFrame = glfwGetTime( );
+		GLfloat currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
-		
+
 		// Check and call events
-		glfwPollEvents( );
-		DoMovement( );
-		
+		glfwPollEvents();
+		DoMovement();
+
 		// Clear the colorbuffer
-		glClearColor( 0.2f, 0.2f, 0.2f, 1.0f );
-		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-		
-		shader.Use( );
-		
-		glm::mat4 view = camera.GetViewMatrix( );
-		glUniformMatrix4fv( glGetUniformLocation( shader.getProgram(), "projection" ), 1, GL_FALSE, glm::value_ptr( projection ) );
-		glUniformMatrix4fv( glGetUniformLocation( shader.getProgram(), "view" ), 1, GL_FALSE, glm::value_ptr( view ) );
-		
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shader.Use();
+
+		glm::mat4 view = camera.GetViewMatrix();
+		glUniformMatrix4fv(glGetUniformLocation(shader.getProgram(), "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(glGetUniformLocation(shader.getProgram(), "view"), 1, GL_FALSE, glm::value_ptr(view));
+
 		// Draw the loaded model
-		glm::mat4 model(1);
-		model = glm::translate( model, glm::vec3( 0.0f, -1.75f, 0.0f ) ); // Translate it down a bit so it's at the center of the scene
-		model = glm::scale( model, glm::vec3( 0.2f, 0.2f, 0.2f ) );	// It's a bit too big for our scene, so scale it down
-		glUniformMatrix4fv( glGetUniformLocation( shader.getProgram(), "model" ), 1, GL_FALSE, glm::value_ptr( model ) );
-		world.Draw( shader );
-		
+		glm::mat4	model(1);
+		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// It's a bit too big for our scene, so scale it down
+		glUniformMatrix4fv( glGetUniformLocation(shader.getProgram(), "model"), 1, GL_FALSE, glm::value_ptr(model));
+		world.Draw(shader);
+
 		// Swap the buffers
-		glfwSwapBuffers( window );
+		glfwSwapBuffers(window);
 	}
 
-	glfwTerminate( );
-	return 0;
+	glfwTerminate();
+	return (0);
 }
 
 // Moves/alters the camera positions based on user input
-void DoMovement( )
+void	DoMovement(void)
 {
 	// Camera controls
-	if ( keys[GLFW_KEY_W] || keys[GLFW_KEY_UP] )
+	if (keys[GLFW_KEY_W] || keys[GLFW_KEY_UP])
 	{
-		camera.ProcessKeyboard( FORWARD, deltaTime );
+		camera.ProcessKeyboard(FORWARD, deltaTime);
 	}
 
-	if ( keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN] )
+	if (keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN])
 	{
-		camera.ProcessKeyboard( BACKWARD, deltaTime );
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
 	}
 
-	if ( keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT] )
+	if (keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT])
 	{
-		camera.ProcessKeyboard( LEFT, deltaTime );
+		camera.ProcessKeyboard(LEFT, deltaTime);
 	}
 
-	if ( keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT] )
+	if (keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT])
 	{
-		camera.ProcessKeyboard( RIGHT, deltaTime );
+		camera.ProcessKeyboard(RIGHT, deltaTime);
 	}
 }
 
 // Is called whenever a key is pressed/released via GLFW
-void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode )
+void	KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
-	if ( GLFW_KEY_ESCAPE == key && GLFW_PRESS == action )
+	if (GLFW_KEY_ESCAPE == key && GLFW_PRESS == action)
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
 
-	if ( key >= 0 && key < 1024 )
+	if (key >= 0 && key < 1024)
 	{
-		if ( action == GLFW_PRESS )
+		if (action == GLFW_PRESS)
 		{
 			keys[key] = true;
 		}
-		else if ( action == GLFW_RELEASE )
+		else if (action == GLFW_RELEASE)
 		{
 			keys[key] = false;
 		}
 	}
 }
 
-void MouseCallback( GLFWwindow *window, double xPos, double yPos )
+void	MouseCallback(GLFWwindow *window, double xPos, double yPos)
 {
-	if ( firstMouse )
+	if (firstMouse)
 	{
 		lastX = xPos;
 		lastY = yPos;
 		firstMouse = false;
 	}
 
-	GLfloat xOffset = xPos - lastX;
-	GLfloat yOffset = lastY - yPos;  // Reversed since y-coordinates go from bottom to left
+	GLfloat	xOffset = xPos - lastX;
+	GLfloat	yOffset = lastY - yPos;  // Reversed since y-coordinates go from bottom to left
 
 	lastX = xPos;
 	lastY = yPos;
 
-	camera.ProcessMouseMovement( xOffset, yOffset );
+	camera.ProcessMouseMovement(xOffset, yOffset);
 }
