9ad5c055698ec2b592c8c81832b529443346182a
Cornelius NOLTE <cnolte@c3r5s6.wethinkcode.co.za>
2018-09-03 13:22:44 +0200

    fix scoring now follow camera.

--- source/classes/Scoring.class.cpp
+++ source/classes/Scoring.class.cpp
@@ -1,14 +1,14 @@
 #include "Scoring.class.hpp"
-
+#include "glm/gtc/matrix_inverse.hpp"
 Scoring::Scoring(Shader &shader, std::string model)
 {
 	std::cout << "Scoring - Constructor called " << std::endl;
 	this->_shader = &shader;
 	this->text = new Model(model);
-	this->x_trans = 0.0f;
-	this->z_trans = -400.0f;
-	this->y_trans = -100.0f;
-	this->rotate = 180.0f;
+	this->x_trans = -0.125f;
+	this->z_trans = -0.15f;
+	this->y_trans = 0.072f;
+	this->rotate = 90.0f;
 }
 
 
@@ -37,12 +37,13 @@ void Scoring::draw(Camera &camera)
 	// front.y = sin(glm::radians(camera.getPitch()));
 	// front.z = sin(glm::radians(camera.getYaw())) * cos(glm::radians(camera.getPitch()));
 	// std::cout << "X: " << std::to_string(camera.getUp().x) << " | Y: " << std::to_string(camera.getUp().y) << " | Z: " << std::to_string(camera.getUp().z) << std::endl;
-	glm::mat4 model = camera.GetViewMatrix();
-	model = glm::translate(model, glm::vec3(this->x_trans + (camera.getPossition().x * 2), this->y_trans + (camera.getPossition().y * 2), this->z_trans + (camera.getPossition().z * 2))); 	// Translate item
-	model = glm::scale(model, glm::vec3(10.2f, 10.2f, 10.2f));									// scale item
-	// model = glm::rotate(model, glm::radians((front.y * -1) * 50), glm::vec3(0, 1, 0)); 			// where x, y, z is axis of rotation (e.g. 0 1 0)
+	glm::mat4 model = glm::affineInverse(camera.GetViewMatrix());
+	model = glm::translate(model, glm::vec3(this->x_trans, this->y_trans, this->z_trans)); 	// Translate item
+	model = glm::scale(model, glm::vec3(0.002f, 0.002f, 0.002f));									// scale item
+	model = glm::rotate(model, glm::radians(this->rotate), glm::vec3(1, 0, 0)); 			// where x, y, z is axis of rotation (e.g. 0 1 0)
 	// model = glm::rotate(model, glm::radians(front.y), glm::vec3(0, 1, 0)); 			// where x, y, z is axis of rotation (e.g. 0 1 0)
-	// model = glm::rotate(model, glm::radians(front.z), glm::vec3(0, 0, 1)); 			// where x, y, z is axis of rotation (e.g. 0 1 0)
+	// model = glm::rotate(model, glm::radians(camera.getFront().z * camera.getPossition().z), glm::vec3(1, 0, 0)); 			// where x, y, z is axis of rotation (e.g. 0 1 0)
+	// model *= camera.GetViewMatrix();
 	glUniformMatrix4fv(glGetUniformLocation(this->_shader->getProgram(), "model"), 1, GL_FALSE, glm::value_ptr(model));
 	this->text->Draw(*this->_shader);
 }
